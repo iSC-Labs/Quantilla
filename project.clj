1b08ea784663822a334ba4d323a10e6d6ed5a84a
m1hawkgsm <fatbumondastreet@gmail.com>
2015-01-21 22:27:20 -0500

    Basic pieces.

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject
+  quantilla
+  "0.1.0-SNAPSHOT"
+  :description
+  "Quant Analytics for Stevens SMIF"
+  :url
+  "http://quantilla.net"
+  :dependencies
+  [
+    [http-kit "2.1.19"]
+    [org.clojure/clojure "1.6.0"]
+    [ring-server "0.3.1"]
+    [mysql/mysql-connector-java "5.1.6"]
+    [com.taoensso/timbre "3.3.1"]
+    [korma "0.4.0"]
+    ])
